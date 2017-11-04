1961a0788bf1fe505ff8e2dfe9bb4ceae6bcdac9
Tapojit <tapojitdebnath@gmail.com>
2017-11-04 03:24:23 -0400

    Add existing file

--- scripts/beforeinstall.bash
+++ scripts/beforeinstall.bash
@@ -3,7 +3,7 @@
 # I want to make sure that the directory is clean and has nothing left over from
 # previous deployments. The servers auto scale so the directory may or may not
 # exist.
-if [ -d /var/www/html/codes and scripts ]; then
-    rm -rf /var/www/html/codes and scripts
+if [ -d /var/www/html/codes\ and\ scripts ]; then
+    rm -rf /var/www/html/codes\ and\ scripts
 fi
-mkdir -vp /var/www/html/codes and scripts
+mkdir -vp /var/www/html/codes\ and\ scripts
