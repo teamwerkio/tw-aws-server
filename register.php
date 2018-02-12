e572938e1fda1b4527ca7283836b812f1b8fa7e4
Tapojit <tapojitdebnath@gmail.com>
2018-02-11 20:13:19 -0500

    index and images added

--- register.php
+++ register.php
@@ -56,7 +56,7 @@
 		<header id="header" class="site-header">
 			<div class="container">
 				<div class="site-brand">
-					<a href="landing.php"><img src="../images/assets/logo.png" style="width: 205px; height: 40px;" alt=""></a>
+					<a href="index.php"><img src="../images/assets/logo.png" style="width: 205px; height: 40px;" alt=""></a>
 				</div><!-- .site-brand -->
 				<div class="right-header">					
 					<!-- <nav class="main-menu">
