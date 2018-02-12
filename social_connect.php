b269f29de16d63dbc210f5eafd2d0cdf2c6fa775
Tapojit <tapojitdebnath@gmail.com>
2018-02-11 19:35:51 -0500

    resolving merge

--- social_connect.php
+++ social_connect.php
@@ -21,13 +21,22 @@
 <body class="campaign-detail">
 
 <div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.12&appId=111072682828913&autoLogAppEvents=1';
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>		
+<script>window.fbAsyncInit = function() {
+    FB.init({
+      appId      : "1841177166173972",
+      xfbml      : true,
+      version    : 'v2.12'
+    });
+    FB.AppEvents.logPageView();
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));</script>		
 
 		<main id="main" class="site-main" style="margin-bottom: 0px; padding-bottom: 0px;">
 			<div class="container">
